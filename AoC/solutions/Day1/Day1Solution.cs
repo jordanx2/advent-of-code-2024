971db8bb2fdd10ddb5909f3ecce2977a2d3caae0
Jordan Murray <jordanmurray@Jordans-MacBook-Air.local>
2025-01-15 20:27:22 +0000

    initial commit and day 1 solution

---
+++ AoC/solutions/Day1/Day1Solution.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+
+namespace Day1;
+class Day1Solution 
+{
+  static void Main(string[] args)
+  {
+    try 
+    {
+      string[] lines = File.ReadAllLines("/Users/jordanmurray/Workspaces/advent-of-code/AoC/res/day1.txt");
+      List<int> element1 = new List<int>();
+      List<int> element2 = new List<int>();
+
+      foreach (var line in lines)
+      {
+        var numbers = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        element1.Add(int.Parse(numbers[0]));
+        element2.Add(int.Parse(numbers[1]));
+      }
+
+      element1.Sort();
+      element2.Sort();
+
+      int partOneTotal = element1.Zip(element2, (x, y) => Math.Abs(x - y)).ToArray().Sum();
+      var rightCount = element2.GroupBy(x => x).ToDictionary(g => g.Key, g => g.Count());
+      int partTwoTotal = element1.Sum(x => rightCount.GetValueOrDefault(x, 0) * x);
+        
+      Console.WriteLine("Part one: " + partOneTotal);
+      Console.WriteLine("Part two: " + partTwoTotal);
+
+    } catch(Exception e)
+    {
+      Console.WriteLine("Exception: " + e.Message);
+    }
+  }
+}
---
+++ AoC/solutions/Day2/Day2Solution.cs
@@ -0,0 +1,8 @@
+namespace Day2;
+class Day2Solution 
+{
+  static void Main(string[] args)
+  {
+    Console.WriteLine("hello world");
+  }
+}
